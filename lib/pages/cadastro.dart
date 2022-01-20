566c0d61cf015920fa6c6e4bd079215b3bf70538
Marcell Santana de Deus <marcellsantana@gmail.com>
2022-01-20 15:31:00 -0300

    Add Figura e botao na tela de cadastro

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -10,15 +10,32 @@ class Cadastro extends StatefulWidget {
 class _CadastroState extends State<Cadastro> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Cadastro Page',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 100,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+    return Center(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 50, 0, 10),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('imagens/dog.png'),
+              radius: 100,
+            ),
+          ),
+          ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.grey),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      const EdgeInsets.fromLTRB(30, 15, 30, 15)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          side: const BorderSide(color: Colors.grey)))),
+              onPressed: () => print("ok"),
+              child: Text(
+                "Alterar",
+                style: TextStyle(fontSize: 25),
+              ))
+        ],
       ),
     );
   }
